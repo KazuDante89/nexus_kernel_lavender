d280b148d86646803fe33c4d7aa52732bfa657eb
NotZeetaa <rodrigo2005contente@gmail.com>
2020-12-25 17:08:03 +0000

    Cam Build
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#5 SMP PREEMPT Tue Dec 22 08:16:22 WET 2020"
+#define UTS_VERSION "#3 SMP PREEMPT Fri Dec 25 17:02:44 WET 2020"
 #define LINUX_COMPILE_BY "Zeetaa"
 #define LINUX_COMPILE_HOST "ZeetaaPrjkt"
 #define LINUX_COMPILER "gcc version 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) "
