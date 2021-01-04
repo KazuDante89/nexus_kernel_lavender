2caf15a795c6471b6b8959400361f4f1080f92af
KazuDante89 <karlemmanueldorvl@gmail.com>
2021-01-04 11:48:14 -0500

    Testing 	modified:   .circleci/build.sh 	modified:   arch/arm/boot/dts/qcom/sdm660.dtsi 	modified:   arch/arm/boot/dts/qcom/sdm660_f7a.dtsi 	modified:   arch/arm64/configs/lavender-perf_defconfig

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -19,7 +19,7 @@ function sendinfo() {
         -d chat_id="$chat_id" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="<b>• Nexus Neuron Kernel Project •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Xiaomi Redmi Note7/7S</b> (lavender)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>${KBUILD_COMPILER_STRING}</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
+        -d text="<b>• Nexus Neuron Kernel Project R4•</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Xiaomi Redmi Note7/7S</b> (lavender)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>${KBUILD_COMPILER_STRING}</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
 }
 # Push kernel to channel
 function push() {
@@ -58,7 +58,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [EAS]-Neuron-Kernel_v0.0.2.zip *
+    zip -r9 [EAS]-Neuron*Kernel*R4_v0.0.zip *
     cd ..
 }
 sendinfo
